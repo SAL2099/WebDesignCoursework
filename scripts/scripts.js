e676468a6607ee1e796365544f55bf6f6b6d8704
SAL2099 <115724102+SAL2099@users.noreply.github.com>
2023-04-18 19:36:41 +0100

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,25 @@
 //Function to login and logout 
 
 function login(){
-    var counter = 2;
-    if(counter % 2 == 0){
-        alert('You have logged in!');
+    //Checks if the user is logged in
+    if(document.getElementById("loginButton").innerHTML=="Log in"){
+        //If the user isn't logged in, check there is an input in the field and log them in. Then change the log in to a log out button.
+        if(document.getElementById("mainLogin").checkValidity()){
+            alert('You have logged in!');
+            document.getElementById("loginButton").innerHTML="Log out";
+        }
+        //If the form isn't filled in, don't log them in.
+        else{
+            alert("Invalid form, email or password missing")
+        }
     }
+
+    //If the user is logged in, log them out and change the button back to a log in.
     else{
-        alert("You have logged out!")
+        alert("You have been logged out!")
+        document.getElementById("loginButton").innerHTML="Log in";
     }
-        counter += 1;
+
 }
 
 
@@ -17,3 +28,24 @@ function login(){
 function sendForm(){
     document.getElementById("form_outcome").innerHTML="Your form was successfully transmitted!"
 }
+
+
+
+//Function for submitting an event
+
+function submitEvent(){
+    var title = document.forms["event"]["eventTitle"].value;
+    var date = document.forms["event"]["eventDate"].value;
+    if(title==""){
+        alert("An event must have a title!");
+    }
+    else{
+        if(date.length == 0){
+            alert("An event must have a date!");
+        }
+        else{
+            alert("Event submitted! We shall be in touch soon, expect our raven!");
+        }
+    }
+}
+
